eb4e499a967e38ff16dd98f3a6af6aaa7f2643fe
ColePeterson-12 <816427@mystma.org>
2022-09-21 14:19:13 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-#WRITE YOUR CODE IN THIS FILE
+from fileinput import close
+from tkinter import Y
+
+
+def close10(x,y):
+    if abs(10-x)>abs(10-y):
+        return y
+    elif abs(10-x)<abs(10-y):
+        return x
+    else:
+        return 0
+print(close10(5,12))
+print(close10(9,50))
+print(close10(8,12))
